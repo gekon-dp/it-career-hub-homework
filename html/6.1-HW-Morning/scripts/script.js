68ebdc110772cfb74c74ab8b745ad869aa5407e9
gekon-dp <geiko.kostya@gmail.com>
2026-08-06 21:19:03 +0000

    making first javascript homework

---
+++ html/6.1-HW-Morning/scripts/script.js
@@ -0,0 +1,9 @@
+const firstName = "Kostiantyn";
+const lastName = "Heiko";
+let greeting = `Привет, ${firstName} ${lastName}`;
+console.log(greeting);
+
+let sentencePart1 = "I love";
+let sentencePart2 = "web development";
+let completeSentence = `${sentencePart1} ${sentencePart2}`;
+console.log(completeSentence);
